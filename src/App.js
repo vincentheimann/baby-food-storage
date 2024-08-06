517f6c33a96e05fe7402d8bb82968a1bf0a9a00b
vincentheimann <heimvin@gmail.com>
2024-08-06 08:41:21 +0200

    Cleanup unused files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ThemeProvider, CssBaseline } from "@mui/material";
 import theme from "./theme";
 import Home from "./pages/Home";
-import AddFood from "./pages/AddFood";
 import Dashboard from "./pages/Dashboard";
 import Notifications from "./pages/Notifications";
 import BacConfig from "./pages/BacConfig";
@@ -22,7 +21,6 @@ const App = () => {
             <TopBar />
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/add-food" element={<AddFood />} />
               <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/notifications" element={<Notifications />} />
               <Route path="/config-bacs" element={<BacConfig />} />
