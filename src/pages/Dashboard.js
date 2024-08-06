d49c0c9f7bc1fdab03f7cacac733b52a44bc8fb1
vincentheimann <heimvin@gmail.com>
2024-08-06 12:15:40 +0200

    Add a Pie Chart in the Dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -12,10 +12,41 @@ import BacCard from "../components/BacCard";
 import AlimentList from "../components/AlimentList";
 import { BacContext } from "../context/BacContext";
 import { AlimentContext } from "../context/AlimentContext";
+import { PieChart } from "@mui/x-charts/PieChart";
 
 const Dashboard = () => {
   const { bacs } = useContext(BacContext);
   const { aliments } = useContext(AlimentContext);
+
+  const getTypeTranslation = (type) => {
+    switch (type) {
+      case "Proteins":
+        return "Protéines";
+      case "Vegetables":
+        return "Légumes";
+      case "Carbs":
+        return "Féculents";
+      case "Fruits":
+        return "Fruits";
+      default:
+        return type;
+    }
+  };
+
+  const getColorByType = (type) => {
+    switch (type) {
+      case "Proteins":
+        return "blue";
+      case "Vegetables":
+        return "green";
+      case "Carbs":
+        return "red";
+      case "Fruits":
+        return "pink";
+      default:
+        return "grey";
+    }
+  };
 
   const filterAlimentsByType = (type) => {
     return aliments.filter((aliment) => aliment.type === type);
@@ -28,12 +59,41 @@ const Dashboard = () => {
     return diffDays <= 7; // Aliments dont la date de péremption est dans 7 jours ou moins
   });
 
+  const alimentTypes = aliments.reduce((acc, aliment) => {
+    acc[aliment.type] = (acc[aliment.type] || 0) + aliment.quantite;
+    return acc;
+  }, {});
+
+  const pieChartData = Object.keys(alimentTypes).map((key, index) => ({
+    id: index,
+    value: alimentTypes[key],
+    label: getTypeTranslation(key),
+    color: getColorByType(key),
+  }));
+
   return (
     <Container maxWidth="lg">
-      <Typography variant="h3" component="h1" gutterBottom>
-        Tableau de bord
-      </Typography>
       <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography variant="h3" component="h1" gutterBottom>
+            Tableau de bord
+          </Typography>
+          <Box mt={5}>
+            <Card>
+              <CardContent>
+                <Typography variant="h4" component="h2" gutterBottom>
+                  Répartition des Types d'Aliments
+                </Typography>
+                <PieChart
+                  series={[{ data: pieChartData }]}
+                  width={400}
+                  height={200}
+                  colors={pieChartData.map((data) => data.color)}
+                />
+              </CardContent>
+            </Card>
+          </Box>
+        </Grid>
         {bacs.map((bac) => (
           <Grid item xs={12} key={bac.id}>
             <BacCard
